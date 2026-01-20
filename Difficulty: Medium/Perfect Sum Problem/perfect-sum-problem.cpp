88ac66ea2a9bf390a947685b708962c681eed954
Bhavya Bhardwaj <bhavya2684qd@gmail.com>
2026-01-20 20:22:06 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Perfect Sum Problem/perfect-sum-problem.cpp
@@ -0,0 +1,19 @@
+class Solution {
+  public:
+    int perfectSum(vector<int>& arr, int target) {
+        // code here
+        int n=arr.size();
+        vector<vector<int>> t(n+1,vector<int> (target+1,0));
+        t[0][0]=1;
+        for(int i=1;i<n+1;i++){
+            for(int j=0;j<target+1;j++){
+                if(arr[i-1]<=j){
+                    t[i][j]=t[i-1][j] + t[i-1][j-arr[i-1]];
+                }else{
+                    t[i][j]=t[i-1][j];
+                }
+            }
+        }
+        return t[n][target];
+    }
+};
